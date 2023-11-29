d63ffa4f4b8cd4b18e61247bd1505e19efbc0b4f
youssefhj <hajibyoussef02@gmail.com>
2023-11-29 14:29:28 -0800

    task13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -9,7 +9,7 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *p1, *p2;
+	listint_t *p1 = *head;
 	listint_t *node;
 
 	node = (listint_t *)malloc(sizeof(listint_t));
@@ -19,38 +19,23 @@ listint_t *insert_node(listint_t **head, int number)
 	node->n = number;
 	node->next = NULL;
 	
-	if (*head == NULL)
-	{
-		*head = node;
-		return (node);
-	}
-
-	p1 = *head;
-	p2 = (*head)->next;
-
-	while (p2)
-	{
-		if (number == p1->n)
-			return (node);
-
-		if (number > p1->n && number < p2->n)
-		{
-			p1->next = node;
-			node->next = p2;
-			return (node);
-		}
-		p1 = p2;
-		p2 = p2->next;
-	}
-	
-	if (p1->n < number)
-	{
-		p1->next = node;
-	}
-	else if (p1->n > number)
+	if (p1 == NULL || node->n < p1->n)
 	{
 		node->next = p1;
-		(*head)->next = node;
+		return (*head = node);
 	}
-	return (node);
+
+
+	while (p1)
+	{
+		if (p1->next == NULL || node->n < p1->next->n)
+		{
+			node->next = p1->next;
+			p1->next = node;
+			return (node);
+		}
+		p1 = p1->next;
+	}
+	
+	return (NULL);
 }
