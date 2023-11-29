8e10025c179c31af4f068b4b05f634f29d516ece
youssefhj <hajibyoussef02@gmail.com>
2023-11-29 04:36:16 -0800

    task10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if list has a cycle
+ * @list: list
+ *
+ * Return: 0 if there is no cycle 1 otherwize
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *pre, *curr;
+
+	pre = curr = list;
+	while (curr)
+	{
+		curr = curr->next;
+		if (curr == pre)
+			return (1);
+
+		pre = curr;
+	}
+
+	return (0);
+}
