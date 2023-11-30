1d6efae1fcf22ce1bcf9d2825292721e2feec3ae
youssefhj <hajibyoussef02@gmail.com>
2023-11-30 14:05:27 -0800

    task3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    it = 0
+    for i in range(len(sys.argv) - 1):
+        it = it + len(sys.argv[i + 1])
+    print("{}".format(it))
