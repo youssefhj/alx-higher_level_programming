13eb667b93a0ee84bcf5f090c9b4e67b83cc046b
youssefhj <hajibyoussef02@gmail.com>
2023-12-07 14:40:34 -0800

    task12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,6 +6,6 @@ def roman_to_int(roman_string):
     n = 0
     d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for r in reversed(roman_string):
-        n  = d[r]
-        total += n if total < n * 5 else n
+        n = d[r]
+        total += n if total < n * 5 else -n
     return total
