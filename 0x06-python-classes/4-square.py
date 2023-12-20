30fce3ac9f27ac97fc70404ea46700b0051c20df
youssefhj <hajibyoussef02@gmail.com>
2023-12-20 12:29:25 -0800

    task4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Define a square"""
+
+    def __init__(self, size=0):
+        """Constructor
+
+        Args:
+            size: length of side of the square
+
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+    
+    @property
+    def size(self):
+        """Property for the length of a side of this square
+
+        Raises:
+            TypeError: if the size is not an integer
+            ValueError: if size is less than 0
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+
+    def area(self):
+        """Area of this square
+
+        Returns:
+            The size squared
+        """
+        return self.__size ** 2
