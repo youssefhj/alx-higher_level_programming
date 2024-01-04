9a45a0d1d0da124b70de2546bdef2fe7ced836d8
youssefhj <hajibyoussef02@gmail.com>
2024-01-04 07:38:05 -0800

    task1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,8 +8,8 @@ class Rectangle:
     """Definition of the Rectangle class"""
     def __init__(self, width=0, height=0):
         """Constructor"""
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
