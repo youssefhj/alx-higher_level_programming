34d4fd46e76dbd43b54cf86ab9feeeb7640dd96f
youssefhj <hajibyoussef02@gmail.com>
2024-01-04 09:37:49 -0800

    task8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -75,11 +75,8 @@ class Rectangle:
         """Return the biggest area between rect_1 and rect_2"""
         if not isinstance(rect_1, Rectangle):
             TypeError("rect_1 must be an instance of Rectangle")
-
         if not isinstance(rect_2, Rectangle):
             TypeError("rect_2 must be an instance of Rectangle")
-
         if rect_1.area() >= rect_2.area():
             return rect_1
-
         return rect_2
