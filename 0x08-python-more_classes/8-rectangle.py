f0ddb448c10e1fec0da49fad15ec30b834e9c614
youssefhj <hajibyoussef02@gmail.com>
2024-01-04 09:32:32 -0800

    task8

---
+++ 0x08-python-more_classes/8-rectangle.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+"""
+Defines a Rectangle class
+"""
+
+
+class Rectangle:
+    """Respresent a Rectangle"""
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """Constructor"""
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    def __del__(self):
+        """Destructor """
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """area getter"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """perimeter of rectangel"""
+        if self.__width > 0 and self.__height > 0:
+            return 2 * (self.__width + self.__height)
+        return 0
+
+    def __str__(self):
+        """String representation of Rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        return ((str(self.print_symbol) * self.__width + "\n") * self.__height)[:-1]
+
+    def __repr__(self):
+        """Representation of rectange object"""
+        return "Rectange({:d}, {:d})".format(self.__width, self.__height)
+
+    def bigger_or_equal(rect_1, rect_2):
+        """Return the biggest area between rect_1 and rect_2"""
+        if type(rect_1) is not Rectangle:
+            TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) is not Rectangle:
+            TypeError("rect_2 must be an instance of Rectangle")
+
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+
+        return rect_2
