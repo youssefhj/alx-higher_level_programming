f2d5cfabebb997881c80edd8aaea3e850e019483
youssefhj <hajibyoussef02@gmail.com>
2024-01-04 07:51:03 -0800

    task1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-
 """
-Rectangle class module
+Defines a Rectangle class
 """
 
 class Rectangle:
-    """Definition of the Rectangle class"""
-
+    """Respresent a Rectangle"""
     def __init__(self, width=0, height=0):
         """Constructor"""
         self.width = width
